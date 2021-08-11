5d7130593fa57034044a57086a7789b4d9308cb7
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:02:54 -0300

    Uri 1008 Salario

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero, horas;
+            double valorHora, salario;
+
+            numero = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            horas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = valorHora * horas;
+
+            Console.WriteLine("NUMBER = " + numero);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+             }
+    }   
+}
